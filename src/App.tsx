1da46cd32ed758b4de37076b51897f3c4627eb0e
ngohuycan99 <cannhhe130043@gmail.com>
2022-08-04 13:35:52 +0700

    add twin macro

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import tw, { theme } from 'twin.macro'
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <StyledWithPropsDiv color="blue">
         Styled With Props Div
       </StyledWithPropsDiv>
+      <TwinMacroDiv>Twin Macro Div</TwinMacroDiv>
+      <ThemeOfTwinMacroDiv>Theme Of Twin Macro Div</ThemeOfTwinMacroDiv>
     </div>
   )
 }
@@ -28,4 +31,11 @@ const StyledWithAttrsDiv = styled.div.attrs({
 
 const StyledWithPropsDiv = styled.div<{ color: string }>`
   color: ${props => props.color};
+`
+
+const TwinMacroDiv = styled.div`
+  ${tw`text-yellow-500`}
+`
+const ThemeOfTwinMacroDiv = styled.div`
+  color: ${theme`colors.purple.500`};
 `
