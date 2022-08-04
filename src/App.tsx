b95cd2e04dff2812204e94e52ba01d84eaa0d9d6
ngohuycan99 <cannhhe130043@gmail.com>
2022-08-04 13:31:58 +0700

    add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function App() {
   return (
     <div>
       <span className="text-green-500">React base</span>
+      <StyledDiv>Styled Div</StyledDiv>
+      <StyledWithAttrsDiv>Styled With Attrs Div</StyledWithAttrsDiv>
+      <StyledWithPropsDiv color="blue">
+        Styled With Props Div
+      </StyledWithPropsDiv>
     </div>
   )
 }
 
 export default App
+
+const StyledDiv = styled.div`
+  width: 50px;
+  height: 100px;
+  background-color: red;
+`
+
+const StyledWithAttrsDiv = styled.div.attrs({
+  className: 'text-red-500',
+})``
+
+const StyledWithPropsDiv = styled.div<{ color: string }>`
+  color: ${props => props.color};
+`
